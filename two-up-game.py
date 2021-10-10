df8a8aca116597756af9c82b42d0fe21124d5429
madelaine2850 <88865425+madelaine2850@users.noreply.github.com>
2021-10-10 11:29:13 +0800

    Update two-up-game.py

--- two-up-game.py
+++ two-up-game.py
@@ -2,7 +2,23 @@ import random
 
 from breezypythongui import EasyFrame
 
-class Game:
+
+class Game(EasyFrame):
+
+    def __init__(self):
+        EasyFrame.__init__(self, title="Two Up Game")
+
+        self.askBet = self.addLabel(text="Please place your bet (HH, HT, or TT):\n",
+                                    row=0, column=0)
+
+        self.answerField = self.addTextField(text="",
+                                             row=1, column=0)
+
+        self.submitButton = self.addButton(text="Submit",
+                                           row=2, column=0, command=self.game)
+
+        self.result = self.addTextField(text="",
+                                        row=3, column=0)
 
     @staticmethod
     def flip_coin():
@@ -12,24 +28,13 @@ class Game:
         result = coin1 + coin2
         return result
 
-    @staticmethod
-    def game():
-        operation = 1
+    def game(self):
         score = 0
-        while operation == 1:
-            guess = input("Please place your bet (HH, HT, or TT):\n")
-            if guess == Game.flip_coin():
-                print(f"{guess} You Win!\n")
-                score += 1
-            else:
-                print(f"{Game.flip_coin()} You lose!\n")
-            print(f"Your score is {score} now.")
-            again = input("Do you want another round? (Y or N)\n")
-            if again == "Y":
-                pass
-            else:
-                operation = 0
+        if self.answerField.getText() == Game.flip_coin():
+            answer = f"{self.answerField.getText()} You Win!\n"
+        else:
+            answer = f"{Game.flip_coin()} You lose!\n"
+        self.result.setText(answer)
 
 
-Game.game()
-
+Game().mainloop()
